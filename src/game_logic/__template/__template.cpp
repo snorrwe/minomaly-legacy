3f7b75774e17e2ffcde8a98f991278350e9f60b5
Daniel Kiss <littlesnorrboy@gmail.com>
2018-01-03 23:11:38 +0100

    Create a template for example projects

---
+++ src/game_logic/__template/__template.cpp
@@ -0,0 +1,54 @@
+#include "__template.h"
+
+Program::Program(std::shared_ptr<Core> core) : ILogic(core)
+{
+    renderer = engine->getRenderer();
+    input = engine->getInput();
+    subs = Subscriptions{input->onKeyDown([&](auto const& e) {
+        if (e.key.keysym.sym == SDLK_ESCAPE) engine->stop();
+    })};
+}
+
+Program::~Program()
+{
+    for (auto i = subs.begin(); i != subs.end(); ++i)
+    {
+        i->unsubscribe();
+    }
+}
+
+void Program::start()
+{
+    const auto colorKey = Color(0, 0xFF, 0xFF);
+    const auto rectH = 205;
+    const auto rectW = 265 / 4;
+    images = Texture::loadSpritesheet("assets/__template/__template.png", *renderer,
+                                      {
+                                          getRect(0, 0, rectW, rectH),
+                                          getRect(rectW, 0, rectW, rectH),
+                                          getRect(rectW * 2, 0, rectW, rectH),
+                                          getRect(rectW * 3, 0, rectW, rectH),
+                                      },
+                                      true, &colorKey);
+}
+
+SDL_Rect Program::getRect(int x, int y, int w, int h)
+{
+    auto rect = SDL_Rect();
+    rect.x = x;
+    rect.y = y;
+    rect.w = w;
+    rect.h = h;
+    return rect;
+}
+
+void Program::update()
+{ /* TODO: update logic here */
+}
+
+std::shared_ptr<Texture> Program::loadTexture(std::string const& name, bool flag,
+                                              Color const* color)
+{
+    auto result = renderer->loadTexture(name, flag, color);
+    return result;
+}
