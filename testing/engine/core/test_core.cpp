e44e10fc4f6735a6d79421675e0b1382cea8cbd2
Daniel Kiss <littlesnorrboy@gmail.com>
2018-01-05 13:42:35 +0100

    Add a test to check if the engine can be created

--- testing/engine/core/test_core.cpp
+++ testing/engine/core/test_core.cpp
@@ -1,9 +1,19 @@
 #include "core.h"
 #include "gtest/gtest.h"
+#include <memory>
 
 class CoreTest : public ::testing::Test
 {
 public:
+    class TestProgram : public ILogic
+    {
+    public:
+        TestProgram(std::shared_ptr<Core> core) : ILogic(core) {}
+        virtual ~TestProgram() {}
+
+        virtual void start() {}
+        virtual void update() {}
+    };
 };
 
-TEST_F(CoreTest, CanInit) {}
+TEST_F(CoreTest, CanCreateActual) { Core::create<CoreTest::TestProgram>("...", 0, 0); }
