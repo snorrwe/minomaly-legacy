7bb8cd2454aa1e0266ce7cec8dec7dd5c97b29a7
Daniel Kiss <littlesnorrboy@gmail.com>
2018-02-15 20:54:08 +0100

    Add a few more tests to Json parse

--- testing/common/test_json_serializer.cpp
+++ testing/common/test_json_serializer.cpp
@@ -34,3 +34,21 @@ TEST_F(JsonSerializerTests, CanReadJsonIntoObject)
     EXPECT_EQ(result.color, "red");
     EXPECT_EQ(result.size, -25);
 }
+
+TEST_F(JsonSerializerTests, ThrowsOnIncompleteJson)
+{
+    const auto json = "{\"color\": \"red\"}";
+    EXPECT_THROW(Json::parse<Apple>(json), Json::ParseError);
+}
+
+TEST_F(JsonSerializerTests, ThrowsParseErrorOnInvalidJson)
+{
+    auto json = "{asd \"color\": \"red\",\"size\": -25\n}";
+    EXPECT_THROW(Json::parse<Apple>(json), Json::ParseError);
+
+    json = "{\"color\"asd: \"red\",\"size\": -25\n}";
+    EXPECT_THROW(Json::parse<Apple>(json), Json::ParseError);
+
+    json = "{\"color\"asd: \"red\",\"size\": -2asd5\n}";
+    EXPECT_THROW(Json::parse<Apple>(json), Json::ParseError);
+}
