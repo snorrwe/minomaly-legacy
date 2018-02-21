da4f078f9ba1bcfa2b6cc1bff7f3b7f310d6f488
unknown <littlesnorrboy@gmail.com>
2018-02-21 17:30:53 +0100

    Working on the json parse implementation

--- minomaly/common/json/json.h
+++ minomaly/common/json/json.h
@@ -1,8 +1,5 @@
 #pragma once
-#include "p_json_error.h"
 #include "p_json_utils.h"
-#include <sstream>
-#include <string>
 
 namespace Mino::Json
 {
@@ -12,55 +9,18 @@ constexpr auto property(T Class::*member, const char* name)
     return Private::PropertyImpl<Class, T>{member, name};
 }
 
-template <typename FwIt>
-struct Parser
-{
-    enum ParseState
-    {
-        Default,
-        Key,
-        Value
-    };
-
-    /**
-     *   Parse value T
-     *   T has to be default contructable
-     *   and type T must have a static member function named 'jsonProperties'
-     *   that returns the json properties to be parsed
-     *   parsed properties must be able to beset by the parse method
-     *   (declare them as public)
-     */
-    template <typename T>
-    T parse(FwIt begin, FwIt end)
-    {
-        auto result = T{};
-        // TODO: create property pointer / name / type lookup table
-        auto state = ParseState::Default;
-        for (; begin != end; ++begin)
-        {
-            switch (state)
-            {
-            case ParseState::Default:
-                break;
-            case ParseState::Key:
-                break;
-            case ParseState::Value:
-                auto valueStream = std::stringstream{};
-                // TODO ...
-                // get Type from the property table
-                Private::parseValue<int>(valueStream);
-                break;
-            }
-        }
-
-        return result;
-    }
-};
-
+/**
+ *   Parse value T
+ *   T has to be default contructable
+ *   and type T must have a static member function named 'jsonProperties'
+ *   that returns the json properties to be parsed
+ *   parsed properties must be able to beset by the parse method
+ *   (declare them as public)
+ */
 template <typename T, typename FwIt>
 T parse(FwIt begin, FwIt end)
 {
-    auto parser = Parser<FwIt>{};
-    return parser.parse<T>(begin, end);
+    auto parser = Private::ParseImpl<FwIt>{};
+    return parser.parse<T>(Private::Type<T>{}, begin, end);
 }
 }
--- minomaly/common/json/p_json_utils.h
+++ minomaly/common/json/p_json_utils.h
@@ -1,4 +1,7 @@
 #pragma once
+#include "p_json_error.h"
+#include <sstream>
+#include <string>
 
 namespace Mino::Json::Private
 {
@@ -18,14 +21,51 @@ struct PropertyImpl
 };
 
 template <typename T>
-T parseValue(std::stringstream& stream)
+struct Type
 {
-    return T{}; // TODO
-}
+};
 
-template <>
-int parseValue<int>(std::stringstream& stream)
+template <typename FwIt>
+struct ParseImpl
 {
-    return 0; // TODO
-}
+    enum ParseState
+    {
+        Default,
+        Key,
+        Value
+    };
+
+    template <typename T>
+    T parse(Type<T>, FwIt begin, FwIt end)
+    {
+        auto result = T{};
+        // TODO: create property pointer / name / type lookup table
+        state = ParseState::Default;
+        for (; begin != end; ++begin)
+        {
+            switch (state)
+            {
+            case ParseState::Default:
+                break;
+            case ParseState::Key:
+                break;
+            case ParseState::Value:
+                break;
+            }
+        }
+        return result;
+    }
+
+    int parse(Type<int>, FwIt begin, FwIt end)
+    {
+        auto stream = std::stringstream{};
+        /*for (; begin != end and *begin != ','; ++begin)
+        {
+            stream
+        }*/
+        return 0; // TODO
+    }
+
+    ParseState state = ParseState::Default;
+};
 }
